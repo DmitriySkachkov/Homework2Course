b7acc69d462fda979c15e4fe2e8f154a68d7ef83
DmitriySkachkov <Dimonskachkov96@gmail.com>
2025-05-17 12:05:40 +0300

    Added file JavaScript

---
+++ script.js
@@ -0,0 +1,62 @@
+// 1 задание
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// 2 задание
+const firstIphoneRealeseYear = 2007;
+alert(`Первый iPhone был выпущен в ${firstIphoneRealeseYear} году`);
+
+// 3 задание
+const creatorOfJavaScript = "Брендан Эйх";
+alert(`Создатель JavaScript ${creatorOfJavaScript}`);
+
+// 4 задание
+const x = 10;
+const y = 2;
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+alert(
+    `Сумма: ${x} + ${y} = ${sum}\n` +
+    `Разность: ${x} - ${y} = ${difference}\n` +
+    `Произведение: ${x} * ${y} = ${product}\n` +
+    `Частное: ${x} / ${y} = ${quotient}`
+);
+
+// 5 задание
+const result = y**5;
+alert(`2 в 5-й степени: ${result}`);
+
+// 6 задание
+a = 9;
+let b = 2;
+const reminder = a % b;
+alert(`Остаток от деления 9 на 2: ${reminder}`)
+
+// 7 задание
+let num = 1;
+num += 5;
+num -=  3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+// 8 задание
+let age = Number(prompt('Сколько Вам лет'));
+alert(`${age} - Вам столько лет` )
+
+//9 задание
+let user = {
+  name: String("Дмитрий"),  
+  age: Number(28),          
+  isAdmin: Boolean(true)     
+};
+
+//10 задание
+let name = String(prompt('Какое у Вас имя?'));
+alert(`Привет, ${name}`);

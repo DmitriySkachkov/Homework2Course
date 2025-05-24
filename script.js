882a6327433093e7b7bf4411e583e212ee6defb0
DmitriySkachkov <Dimonskachkov96@gmail.com>
2025-05-24 12:50:42 +0300

    Added tasks JavaScript

--- script.js
+++ script.js
@@ -64,3 +64,73 @@ alert(`Привет, ${name}`);
 */
 
 // ДОМАШНЯЯ РАБОТА 3
+//1 задание
+let password = 'пароль';
+let userPassword = prompt ('Введите пароль');
+if (userPassword===password) {
+  alert ('Пароль введен верно');
+} else {
+  alert ('Пароль введен неправильно');
+}
+
+//2 задание
+function checkNumber(c) {
+    if (c > 0 && c < 10) {
+        console.log(`При c = ${c} - Верно`);
+    } else {
+        console.log(`При c = ${c} - Неверно`);
+    }
+}
+checkNumber(0);    
+checkNumber(10);   
+checkNumber(-3);   
+checkNumber(2);  
+
+//3 задание
+let d = 120;  
+let e = 50;   
+
+if (d > 100 || e > 100) {
+    console.log("Верно");  
+} else {
+    console.log("Неверно"); 
+}
+
+//4 задание
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
+
+//5 задание
+let monthNumber = Number(prompt('Введите номер месяца')); 
+
+if (monthNumber < 1 || monthNumber > 12) {
+    console.log("Некорректный номер месяца. Введите число от 1 до 12.");
+} else {
+    let season;
+    
+    switch (monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            season = "зима";
+            break;
+        case 3:
+        case 4:
+        case 5:
+            season = "весна";
+            break;
+        case 6:
+        case 7:
+        case 8:
+            season = "лето";
+            break;
+        case 9:
+        case 10:
+        case 11:
+            season = "осень";
+            break;
+    }
+    
+    console.log(`Месяц с номером ${monthNumber} относится к сезону «${season}»`);
+}
